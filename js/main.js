c25a1e03849e2106a68e9911b0ddeddc602d78f4
BreDi <40304887+Glendi123@users.noreply.github.com>
2021-07-02 00:01:09 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  loop: true,
+  navigation: {
+    nextEl: ".arrow",
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 2,
+    },
+  },
+})
+
+let menuButton = document.querySelector(".menu-button")
+let menu = document.querySelector(".header")
+
+menuButton.addEventListener("click", () => {
+  menuButton.classList.toggle("menu-button-active")
+  menu.classList.toggle("header-active")
+})
